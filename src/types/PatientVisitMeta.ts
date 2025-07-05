b3969439190336934d3cd68ccab5119b8215bbf3
Darren <darren_low42@outlook.sg>
2025-07-05 17:45:25 +0800

    patient queue page

--- src/types/PatientVisitMeta.ts
+++ src/types/PatientVisitMeta.ts
@@ -1,14 +1,15 @@
 export default interface PatientVisitMeta {
-    id?: number
-    vid?: number
-    familyGroup: string
-    regDate: string // ISO date string
-    queueNo: string
-    name: string
-    khmerName: string
-    gender: 'M' | 'F'
-    village: string
-    contactNo: string
-    drugAllergies: string | null
-    sentToId: boolean
+  id?: number
+  vid?: number
+  familyGroup: string
+  regDate: string // ISO date string
+  queueNo: string
+  name: string
+  khmerName: string
+  gender: 'M' | 'F'
+  village: string
+  contactNo: string
+  drugAllergies: string | null
+  sentToId: boolean
+  referralNeeded: boolean
 }
