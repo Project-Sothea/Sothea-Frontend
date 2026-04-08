a955d1c9bbe40ad51864d80add77f14ebb092146
Sai <saibhagatsr@gmail.com>
2026-04-08 20:41:05 +0800

    Add endpoint to fetch users

--- src/features/auth/types/Profiles.tsx
+++ src/features/auth/types/Profiles.tsx
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-// Profile definitions
-export const profiles = [
-  { username: 'admin', name: 'Admin' },
-  { username: 'user', name: 'User' },
-  { username: 'reg', name: 'Reg' },
-  { username: 'reg_id', name: 'Reg (ID)' },
-  { username: 'va', name: 'VA' },
-  { username: 'fall_risk', name: 'Fall Risk' },
-  { username: 'dental', name: 'Dental' },
-  { username: 'physiotherapy', name: 'Physiotherapy' },
-  { username: 'pharmacy_packer', name: 'Pharmacy packer' },
-  { username: 'pharmacy_dispenser', name: 'Pharmacy dispenser' },
-  { username: 'dr_ron', name: 'Dr Ron' },
-  { username: 'dr_joseph', name: 'Dr Joseph' },
-  { username: 'dr_hh', name: 'Dr HH' },
-  { username: 'dr_kevan', name: 'Dr Kevan' },
-  { username: 'dr_sherryl', name: 'Dr Sherryl' },
-  { username: 'dr_jonathan', name: 'Dr Jonathan' },
-  { username: 'dr_lauren', name: 'Dr Lauren' },
-  { username: 'dr_celeste', name: 'Dr Celeste' },
-  { username: 'dr_barbara', name: 'Dr Barbara' }
-]
-=======
 export interface LoginResponse {
   token: string
 }
@@ -30,4 +6,3 @@ export interface User {
   username: string
   name: string
 }
->>>>>>> Stashed changes
